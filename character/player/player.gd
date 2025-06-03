e042178a4b10a5e434c6e1140dd9ca9b4df15853
almic <mick.ashton@flare-esports.net>
2025-06-03 12:59:57 -0600

    Implement stepping in player controller

--- character/player/player.gd
+++ character/player/player.gd
@@ -1,5 +1,9 @@
+@tool
+
 class_name Player extends CharacterBody3D
 
+@onready var collider: CollisionShape3D = %collider
+@onready var camera_target: Node3D = %CameraTarget
 @onready var camera_3d: Camera3D = %Camera3D
 @onready var hurtbox: HurtBox = %Hurtbox
 @onready var weapon: Weapon = %weapon
@@ -11,8 +15,12 @@ class_name Player extends CharacterBody3D
 
 @export_group("Movement")
 @export var gravity: float = 9.81
-@export var jump_power: float = 5
+@export var jump_power: float = 5.0
 @export var look_speed: float = 0.55
+
+@export_subgroup("Camera", "camera")
+@export var camera_smoothing: float = 10.0
+@export var camera_lag_distance: float = 1.0
 
 @export_subgroup("Moving", "move")
 @export var move_acceleration: float = 20
@@ -21,11 +29,21 @@ class_name Player extends CharacterBody3D
 @export var move_air_ctl: float = 0.25
 @export var move_turn_speed_keep: float = 0.67
 
+@export_subgroup("Stepping", "step")
+@export var step_up_max: float = 0.4
+@export var step_down_max: float = 0.3
+
 @export_group("Controls")
 @export var jump: GUIDEAction
 @export var look: GUIDEAction
 @export var move: GUIDEAction
 @export var fire_primary: GUIDEAction
+
+
+var is_grounded: bool = true
+var was_grounded: bool = true
+var just_stepped: bool = false
+
 
 func _ready() -> void:
     hurtbox.enable()
@@ -36,35 +54,50 @@ func _ready() -> void:
     weapon.set_trigger(fire_primary)
 
 func _process(_delta: float) -> void:
+    if Engine.is_editor_hint():
+        camera_3d.global_transform = camera_target.global_transform
+        return
+
     rotation_degrees.y -= look.value_axis_2d.x * look_speed
+    camera_3d.rotation.y = rotation.y
     camera_3d.rotation_degrees.x = clampf(
         camera_3d.rotation_degrees.x - look.value_axis_2d.y * look_speed,
         -89, 89
     )
 
 func _physics_process(delta: float) -> void:
+    if Engine.is_editor_hint():
+        return
+
+    was_grounded = is_grounded
+
     var move_length: float = move.value_axis_3d.length()
     var stationary: bool = is_zero_approx(move_length)
 
-    var move_direction
+    var move_direction: Vector2
     var move_dot: float = -2 # -2 is a signal value
     var movement: Vector3
 
     if !stationary:
-        move_direction = move.value_axis_3d / move_length
-        movement = basis * move_direction
+        var direction: Vector3 = move.value_axis_3d / move_length
+        movement = basis * direction
         move_direction = Vector2(movement.x, movement.z) # true direction
         movement *= move_acceleration * delta
     else:
         movement = Vector3.ZERO
 
-    if not is_on_floor():
+    # Stay "on ground" while stepping
+    if not just_stepped and not is_on_floor():
+        is_grounded = false
+
         # Less control in air
         movement *= move_air_ctl
 
         # Gravity
         velocity.y -= gravity * delta
     else:
+        is_grounded = true
+
         # Jumping
         if jump.is_triggered() or jump.is_ongoing():
             velocity.y += jump_power
@@ -110,28 +143,31 @@ func _physics_process(delta: float) -> void:
 
     # Accelerate up to top speed, deccel whichever way
     if !stationary:
-        # For later, if wanted
-        #if move_dot < -1:
-            #move_dot = clampf(
-                #Vector2(velocity.x, velocity.z)
-                        ## .normalized() # not wanted
-                        #.dot(move_direction),
-                #-1, 1
-            #)
-
         movement.x += velocity.x
         movement.z += velocity.z
 
         # Enforce maximum additional speed
-        var len_sqr: float = movement.length_squared()
-        if len_sqr > move_top_speed ** 2:
-            movement = (movement / sqrt(len_sqr)) * move_top_speed
+        movement = movement.limit_length(move_top_speed)
 
         velocity.x = movement.x
         velocity.z = movement.z
 
+    # Step up on input (stationary), use true displacment (movement * delta)
+    var stepped: bool = false
+    if is_grounded and not stationary:
+        stepped = step_up(movement * delta)
+
     # Update position
     move_and_slide()
+
+    # Step down
+    if not just_stepped and not is_grounded and was_grounded and velocity.y <= 0:
+        step_down()
+
+    # Camera anti-jitter
+    smooth_camera(delta)
+
+    just_stepped = stepped
 
 func take_hit(_from: Node3D, _to: HurtBox, _hit: Dictionary, damage: float) -> void:
     # take damage
@@ -141,3 +177,107 @@ func take_hit(_from: Node3D, _to: HurtBox, _hit: Dictionary, damage: float) -> v
         return
 
     print("gah... dead!")
+
+func step_up(movement: Vector3) -> bool:
+    var rid := get_rid()
+    var result := PhysicsTestMotionResult3D.new()
+    var params := PhysicsTestMotionParameters3D.new()
+
+    # Move forward to collision
+    var test_transform := global_transform
+    params.from = test_transform
+    params.motion = movement
+
+    # No hit, no step needed
+    if not PhysicsServer3D.body_test_motion(rid, params, result):
+        return false
+
+    # Save position and remainder
+    test_transform = test_transform.translated(result.get_travel())
+    var remainder: Vector3 = result.get_remainder()
+
+    # Step up
+    params.from = test_transform
+    params.motion = up_direction * step_up_max
+    PhysicsServer3D.body_test_motion(rid, params, result)
+    test_transform = test_transform.translated(result.get_travel())
+
+    # Forward by remainder
+    params.from = test_transform
+    params.motion = remainder
+    PhysicsServer3D.body_test_motion(rid, params, result)
+    test_transform = test_transform.translated(result.get_travel())
+
+    # Slide on walls
+    if result.get_collision_count() > 0:
+        var dir: Vector3 = movement.normalized()
+        var wall_normal: Vector3 = result.get_collision_normal()
+        var ddm: float = dir.dot(wall_normal) / (wall_normal * wall_normal).length()
+        var slide: Vector3 = (dir - ddm * wall_normal).normalized()
+
+        params.from = test_transform
+        params.motion = slide * result.get_remainder().length()
+
+        PhysicsServer3D.body_test_motion(rid, params, result)
+        test_transform = test_transform.translated(result.get_travel())
+
+    # Back down to surface to ensure safe ground
+    params.from = test_transform
+    params.motion = -up_direction * (step_up_max + 0.01)
+
+    # Must hit ground
+    if not PhysicsServer3D.body_test_motion(rid, params, result):
+        return false
+
+    # New "ground" must be below our max step height, very important test!
+    # Otherwise, spheres/ capsules can climb up to `step + (height / 2)`!
+    if result.get_collision_point().y - global_position.y > step_up_max:
+        return false
+
+    # Respect floor_max_angle
+    var space := get_world_3d().direct_space_state
+    var from := result.get_collision_point() + up_direction * step_up_max * 2.0
+    var to := result.get_collision_point() - up_direction * step_up_max * 2.0
+    var raycast := space.intersect_ray(PhysicsRayQueryParameters3D.create(
+            from,
+            to,
+            collision_mask,
+            [rid]
+    ))
+    if not raycast or up_direction.angle_to(raycast.normal) > floor_max_angle:
+        return false
+
+    # Everything passed, shift player up for move_and_slide()
+    test_transform = test_transform.translated(result.get_travel())
+    global_position.y = test_transform.origin.y + 0.001
+
+    return true
+
+func step_down() -> void:
+    var result := PhysicsTestMotionResult3D.new()
+    var params := PhysicsTestMotionParameters3D.new()
+
+    params.from = global_transform
+    params.motion = -up_direction * step_down_max
+    if not PhysicsServer3D.body_test_motion(get_rid(), params, result):
+        return
+
+    position.y += result.get_travel().y
+    apply_floor_snap()
+    is_grounded = true
+
+func smooth_camera(delta: float) -> void:
+    camera_3d.global_position.x = camera_target.global_position.x
+    camera_3d.global_position.z = camera_target.global_position.z
+    #camera_3d.global_position.y = camera_target.global_position.y
+
+    # Anti-jitter and lag limit
+    camera_3d.global_position.y = clampf(
+            lerpf(
+                    camera_3d.global_position.y,
+                    camera_target.global_position.y,
+                    camera_smoothing * delta
+            ),
+            camera_target.global_position.y - camera_lag_distance,
+            camera_target.global_position.y + camera_lag_distance
+    )

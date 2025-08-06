3442c9c62a8c3b094607dc60d5f57bceeb0f7c2a
almic <mick.ashton@flare-esports.net>
2025-08-06 11:56:49 -0600

    Add UI theme and fonts, update UIs
    - Added two fonts, one for numbers and titles, one for
      content text
    - Update UIs to use new theme and fix some layout issues

--- script/hud/hud.gd
+++ script/hud/hud.gd
@@ -1,3 +1,5 @@
+@tool
+
 class_name HUD extends Control
 
 @onready var score_value: Label = %score_value
@@ -8,7 +10,80 @@ class_name HUD extends Control
 @onready var stock_texture: TextureRect = %stock_texture
 @onready var stock_amount: Label = %stock_amount
 
-@onready var ammo_flow: HFlowContainer = %ammo_flow
+@onready var ammo_flow: HBoxContainer = %ammo_flow
+
+
+@export_group("Preview", "_preview")
+
+@export var _preview_weapon: WeaponResource:
+    set(value):
+        if not Engine.is_editor_hint():
+            _preview_weapon = null
+            return
+
+        _preview_weapon = value
+        _preview_weapon._mixed_reserve = _preview_weapon_ammo_mix
+        update_weapon_ammo.call_deferred(_preview_weapon, _preview_stock_ammo)
+
+@export var _preview_weapon_ammo_mix: PackedInt32Array:
+    set(value):
+        if not Engine.is_editor_hint():
+            _preview_weapon_ammo_mix = []
+            return
+
+        _preview_weapon_ammo_mix = value
+        _preview_weapon._mixed_reserve = _preview_weapon_ammo_mix
+        _preview_weapon._simple_reserve_type = _preview_weapon_ammo_reserve_type
+        _preview_weapon._simple_reserve_total = _preview_weapon_ammo_reserve_total
+        update_weapon_ammo.call_deferred(_preview_weapon, _preview_stock_ammo)
+
+@export var _preview_weapon_ammo_reserve_type: int:
+    set(value):
+        if not Engine.is_editor_hint():
+            _preview_weapon_ammo_reserve_type = 0
+            return
+
+        _preview_weapon_ammo_reserve_type = value
+        _preview_weapon._simple_reserve_type = _preview_weapon_ammo_reserve_type
+        update_weapon_ammo.call_deferred(_preview_weapon, _preview_stock_ammo)
+
+@export var _preview_weapon_ammo_reserve_total: int:
+    set(value):
+        if not Engine.is_editor_hint():
+            _preview_weapon_ammo_reserve_total = 0
+            return
+
+        _preview_weapon_ammo_reserve_total = value
+        _preview_weapon._simple_reserve_total = _preview_weapon_ammo_reserve_total
+        update_weapon_ammo.call_deferred(_preview_weapon, _preview_stock_ammo)
+
+@export var _preview_stock_ammo: Dictionary:
+    set(value):
+        if not Engine.is_editor_hint():
+            _preview_stock_ammo = {}
+            return
+
+        _preview_stock_ammo = value
+        update_weapon_ammo.call_deferred(_preview_weapon, _preview_stock_ammo)
+
+
+func _ready() -> void:
+    if not Engine.is_editor_hint():
+        reset.call_deferred()
+
+## Resets all elements and placeholders prior to in-game layout
+func reset() -> void:
+    score_value.text = '0'
+    health_bar.value = 0
+
+    for child in ammo_flow.get_children():
+        ammo_flow.remove_child(child)
+        child.queue_free()
+
+    stock_texture.texture = null
+    stock_amount.text = ''
+
+    weapon_texture.texture = null
 
 
 func update_score(score: int) -> void:
@@ -39,7 +114,7 @@ func update_weapon_ammo(weapon: WeaponResource, ammo_stock: Dictionary) -> void:
     var ammo: Dictionary = weapon.get_supported_ammunition()
 
     if weapon.can_chamber and weapon._chambered_round_type:
-        add_ammo_reserve(weapon._chambered_round_type, ammo)
+        _add_ammo_reserve(weapon._chambered_round_type, ammo)
 
     if weapon.ammo_can_mix:
         var reserve: PackedInt32Array = weapon.get_mixed_reserve()
@@ -49,13 +124,14 @@ func update_weapon_ammo(weapon: WeaponResource, ammo_stock: Dictionary) -> void:
             reserve.reverse()
 
         for type in reserve:
-            add_ammo_reserve(type, ammo)
+            _add_ammo_reserve(type, ammo)
     else:
         var type: int = weapon.get_reserve_type()
         for i in range(weapon.get_reserve_total()):
-            add_ammo_reserve(type, ammo)
+            _add_ammo_reserve(type, ammo)
 
-func add_ammo_reserve(type: int, ammo: Dictionary) -> void:
+func _add_ammo_reserve(type: int, ammo: Dictionary) -> void:
     var round_texture: TextureRect = TextureRect.new()
     round_texture.texture = ammo.get(type).ui_texture
-    ammo_flow.add_child(round_texture)
+    ammo_flow.add_child(round_texture, Engine.is_editor_hint())
+    round_texture.owner = self.owner

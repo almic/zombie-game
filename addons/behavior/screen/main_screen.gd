91b61d6b60177af61b7618db29042c68f0d0331c
almic <mick.ashton@flare-esports.net>
2025-11-01 16:04:50 -0600

    Work on editor for behavior system
    - Layout for behavior editor updated, will be like a file
      editor where many resources can be modified at once.

--- addons/behavior/screen/main_screen.gd
+++ addons/behavior/screen/main_screen.gd
@@ -5,22 +5,28 @@ extends Control
 func _ready() -> void:
     %Menu.new_resource.connect(edit)
     %Menu.goto_settings.connect(show_settings)
+    %Menu.goto_editor.connect(show_editor)
 
     %Settings.goto_menu.connect(show_menu)
+
+    %Editor.goto_menu.connect(show_menu)
 
     show_menu()
 
 
 func edit(res: Resource) -> void:
-    if not %Editor.is_saved():
-        # TODO: popup confirm save, discard, or cancel (describe clearly in text)
-        pass
-
     %Settings.visible = false
     %Menu.visible = false
 
     %Editor.visible = true
     %Editor.edit(res)
+
+
+func show_editor() -> void:
+    %Settings.visible = false
+    %Menu.visible = false
+
+    %Editor.visible = true
 
 
 func show_settings() -> void:

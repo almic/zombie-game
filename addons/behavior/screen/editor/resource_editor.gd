33d3319eaf40eb001ae9d5550599103409afa631
almic <mick.ashton@flare-esports.net>
2025-11-08 13:11:49 -0700

    Work on behavior resource editor, styles
    - Style mind editor expandables
    - Add property names with tooltips to resource editors
    - Fix expandable bug which caused the internal item to not
      be updated correctly when it was added before _ready()
    - Move property editor setup code to resource editor class

--- addons/behavior/screen/editor/resource_editor.gd
+++ addons/behavior/screen/editor/resource_editor.gd
@@ -57,3 +57,85 @@ func on_save() -> void:
 
     is_saved = true
     saved.emit()
+
+static func get_property_editors(
+        resource: BehaviorExtendedResource,
+        properties: PackedStringArray,
+        on_change_func: Callable,
+) -> Array[BehaviorPropertyEditor]:
+    var editors: Array[BehaviorPropertyEditor] = []
+    for prop in resource.get_property_list():
+        if not properties.has(prop.name):
+            continue
+        var editor: BehaviorPropertyEditor = BehaviorPropertyEditor.get_editor_for_property(resource, prop)
+        editor.changed.connect(on_change_func)
+        editors.append(editor)
+    return editors
+
+static func make_property_override_container(
+        editor: BehaviorPropertyEditor
+) -> ExpandableContainer:
+    var container: ExpandableContainer = ExpandableContainer.new()
+    container.set_expandable_separation(0)
+    container.icon_separation = 8
+    container.icon_visible = false
+    container.allow_interaction = false
+    container.theme_type_variation = &'ExpandableTransparent'
+    container.title_theme_variation = &'ExpandableNoBorder'
+
+    var title_bar: Control
+    if editor.resource.base:
+        title_bar = VBoxContainer.new()
+
+        var property_header: HBoxContainer = HBoxContainer.new()
+
+        var property_name: Label = Label.new()
+        property_name.theme_type_variation = &'LabelMono'
+        property_name.text = editor.property.name.capitalize()
+
+        var override_button: CheckButton = CheckButton.new()
+        override_button.text = 'Override'
+        # TODO: button things
+
+        property_header.add_child(property_name)
+        property_header.add_child(override_button)
+
+        var base_default: HBoxContainer = HBoxContainer.new()
+        var base_default_label: Label = Label.new()
+        base_default_label.text = 'Base Value:'
+        base_default_label.theme_type_variation = &'TransparentLabel'
+
+        var base_default_value: Label = Label.new()
+        base_default_value.text = str()
+        base_default_value.theme_type_variation = &'LabelMono'
+
+        base_default.add_child(base_default_label)
+
+        title_bar.add_child(property_header)
+        title_bar.add_child(base_default)
+    else:
+        title_bar = MarginContainer.new()
+        title_bar.add_theme_constant_override(&'margin_top', 4)
+        title_bar.add_theme_constant_override(&'margin_bottom', 4)
+        title_bar.add_theme_constant_override(&'margin_left', 8)
+        title_bar.add_theme_constant_override(&'margin_right', 8)
+
+        var property_name: Label = Label.new()
+        property_name.theme_type_variation = &'LabelMono'
+        property_name.text = editor.property.name.capitalize()
+
+        title_bar.add_child(property_name)
+
+    title_bar.tooltip_text = ' %s ' % editor.property.name
+    container.set_title_control(title_bar)
+
+    var margins: MarginContainer = MarginContainer.new()
+    margins.add_theme_constant_override(&'margin_top', 8)
+    margins.add_theme_constant_override(&'margin_bottom', 8)
+    margins.add_theme_constant_override(&'margin_left', 8)
+    margins.add_theme_constant_override(&'margin_right', 8)
+
+    margins.add_child(editor)
+
+    container.set_expandable_control(margins)
+    return container

17ed17dfd31fa30eee6cccc09578a9bec1586756
almic <mick.ashton@flare-esports.net>
2025-11-27 18:51:15 -0700

    Add gear switch and 0-60 debug for vehicle
    - Can now do 0-60 tests by letting the vehicle rest and
      sleep, then just accelerate until reaching 60MPH
    - Print gear switches

--- vehicle/base/wheeled_jolt_vehicle.gd
+++ vehicle/base/wheeled_jolt_vehicle.gd
@@ -10,7 +10,13 @@ class_name WheeledJoltVehicle extends JoltVehicle
 @export_custom(PROPERTY_HINT_GROUP_ENABLE, '')
 var dbg_enable: bool = false
 
+## When accelerating while stationary (sleeping), start a timer which ends once
+## the vehicle exceeds 60mph and print the timer
+@export var dbg_print_zero_sixty: bool = false
+
 @export var dbg_show_wheel_slip: bool = false
+
+@export var dbg_print_gear_changes: bool = false
 
 
 var _forward: float = 0
@@ -18,6 +24,24 @@ var _right: float = 0
 var _brake: float = 0
 var _handbrake: float = 0
 
+var _zero_sixty_timer: float = -1.0
+
+var _last_gear: int = 0
+
+
+func _ready() -> void:
+    if dbg_enable and dbg_show_wheel_slip:
+        # Make all wheel materials unique copies
+        for w in wheel_nodes:
+            var mesh_instance: MeshInstance3D = w as MeshInstance3D
+            if not mesh_instance:
+                continue
+            var mesh: PrimitiveMesh = mesh_instance.mesh as PrimitiveMesh
+            if not mesh:
+                continue
+            mesh = mesh.duplicate()
+            mesh.material = mesh.material.duplicate()
+            mesh_instance.mesh = mesh
 
 func forward(value: float) -> void:
     _forward = clampf(value, -1.0, 1.0)
@@ -33,6 +57,12 @@ func handbrake(amount: float) -> void:
 
 func get_exit_position() -> Vector3:
     return exit_position.global_position + settings.up
+
+func get_mph() -> float:
+    var controller: WheeledVehicleController = get_controller() as WheeledVehicleController
+    if not controller:
+        return 0
+    return controller.get_speed_mph()
 
 func _reset() -> void:
     _forward = 0
@@ -51,7 +81,6 @@ func _physics_process(delta: float) -> void:
             var wheel_infos: Array[Dictionary] = controller.get_wheel_info()
             var i: int = 0
             for wheel in wheel_nodes:
-
                 if wheel is MeshInstance3D:
                     var info: Dictionary = wheel_infos[i]
                     var mat: StandardMaterial3D = wheel.mesh.surface_get_material(0) as StandardMaterial3D
@@ -63,14 +92,40 @@ func _physics_process(delta: float) -> void:
                         mat.albedo_color = Color(remap(info['slip_long'], 0.0, 0.1, 0.0, 1.0), 1.0, 0.0)
                 i += 1
 
+        if dbg_print_zero_sixty:
+            var mph: float = linear_velocity.length() * 3.6 / 1.609344
+            if _zero_sixty_timer != -1.0:
+                _zero_sixty_timer += delta
+                if _forward <= 0.0:
+                    GlobalWorld.print("0-%.1f: %.3fs" % [mph, _zero_sixty_timer])
+                    _zero_sixty_timer = -1.0
+                elif mph >= 60.0:
+                    GlobalWorld.print("0-60: %.3fs" % _zero_sixty_timer)
+                    _zero_sixty_timer = -1.0
+
+        if dbg_print_gear_changes:
+            var gear: int = controller.get_current_gear()
+            if _last_gear != gear:
+                GlobalWorld.print('Gear: %d -> %d' % [_last_gear, gear])
+                _last_gear = gear
+
     if not (
             is_zero_approx(_forward)
         and is_zero_approx(_right)
         and is_zero_approx(_brake)
         and is_zero_approx(_handbrake)
     ):
+        if (
+                    dbg_enable
+                and dbg_print_zero_sixty
+                and sleeping
+                and _forward > 0.0
+                and _zero_sixty_timer == -1.0
+        ):
+            _zero_sixty_timer = 0.0
         # Wake up when input is received
         sleeping = false
+
 
     controller.set_driver_input(
             _forward,
@@ -78,9 +133,11 @@ func _physics_process(delta: float) -> void:
             _brake,
             _handbrake
     )
+
     #if not is_zero_approx(_forward) and controller.is_any_driven_wheel_slipping():
         #print('slipping!')
     #if not is_zero_approx(input_forward):
         #print(controller.get_current_gear())
         #print(controller.get_current_rpm())
+
     _reset()

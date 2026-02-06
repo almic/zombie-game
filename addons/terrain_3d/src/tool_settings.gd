19b237b678c6115614cc48f61791fab1b39f536e
almic <mick.ashton@flare-esports.net>
2026-02-06 16:11:17 -0700

    Fix order of brushes for Terrain3D

--- addons/terrain_3d/src/tool_settings.gd
+++ addons/terrain_3d/src/tool_settings.gd
@@ -277,10 +277,9 @@ func add_brushes(p_parent: Control) -> void:
 	
 	var dir: DirAccess = DirAccess.open(BRUSH_PATH)
 	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if !dir.current_is_dir() and file_name.ends_with(".exr"):
+		var files: PackedStringArray = dir.get_files()
+		for file_name in files:
+			if file_name.ends_with(".exr"):
 				var img: Image = Image.load_from_file(BRUSH_PATH + "/" + file_name)
 				if img:
 					var value_range: Vector2 = Terrain3DUtil.get_min_max(img)
